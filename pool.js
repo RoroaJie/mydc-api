fb0300f5039371ec763edd4482dcca70950efbe3
ZhengHaoJie <602703774@qq.com>
2019-02-15 10:12:42 +0800

    修改连接池成功

--- pool.js
+++ pool.js
@@ -4,7 +4,7 @@
 const mysql=require('mysql');
 var pool=mysql.createPool({
     host     : process.env.MYSQL_HOST,
-    port     : process.env.MYSQL_PORT,
+    port     : process.env.MYSQL_PORT, 
     user     : process.env.ACCESSKEY,
     password : process.env.SECRETKEY,
     database : 'app_' + process.env.APPNAME,  
